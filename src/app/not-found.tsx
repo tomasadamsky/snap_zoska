da6cc924ce4d7ad9bbc1bf0166b57461fd119c3a
tomasadamsky <tomas.adamsky@gmail.com>
2024-09-25 13:53:51 +0200

    initial

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+
+// src/app/not-found.tsx
+
+
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box"
+
+export default function NotFound() {
+    return (
+        <Box>
+            <Typography> Nič sa tu nenachádza </Typography>
+        </Box>
+    );
+}
